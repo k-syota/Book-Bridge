fc497605c21d6c574840994fcf65f93082ef3d26
Nishimurasyota <syota4723@gamil.com>
2020-03-08 12:38:29 +0000

    fix[ログイン機能]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  # コントローラーに設定して、ログイン済ユーザーのみにアクセスを許可する
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  # デバイス機能実行前にconfigure_permitted_parametersの実行をする。
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
+    #sign_upの際にnameのデータ操作を許。追加したカラム。
+  end
+
+  def after_sign_in_path_for(resource)
+    user_path(resource)
+  end
 end
