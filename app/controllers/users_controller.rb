45d6ec09c7bf44c8022f61a483f709086abadd0d
Nishimurasyota <syota4723@gamil.com>
2020-03-08 13:28:25 +0000

    ユーザー詳細画面を設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,17 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :image, :email)
   end
 end
