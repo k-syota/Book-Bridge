2cbeaf26eb82cde37170e7f27da6022a96e97ade
Nishimurasyota <syota4723@gamil.com>
2020-03-12 06:53:21 +0000

    マイリスト機能を実装

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,6 +4,8 @@ class Book < ApplicationRecord
   has_many :favorites, dependent: :destroy #マイリストとの関連付け
   has_many :reviews, dependent: :destroy #レビューとの関連付け
   belongs_to :author, optional: true#著者との関連付け
-
+    def favorited_by?(user)
+        favorites.where(user_id: user.id).exists?
+    end
   acts_as_taggable
 end
