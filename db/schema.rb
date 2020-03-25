ac4a5cbf02d64e02b868a1ba3a66874dc51af1c0
Nishimurasyota <syota4723@gamil.com>
2020-03-25 03:46:36 +0000

    bookのテーブルにあるuser_idのカラムを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_12_053605) do
+ActiveRecord::Schema.define(version: 2020_03_25_034507) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
@@ -23,7 +23,6 @@ ActiveRecord::Schema.define(version: 2020_03_12_053605) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
     t.integer "author_id"
     t.integer "review_quantity"
   end
